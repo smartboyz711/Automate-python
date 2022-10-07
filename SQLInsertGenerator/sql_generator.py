5e4cdfb3e06b950419b35a66963960136a2d8e1b
theedp49 <theedp49@aware.postbox.in.th>
2022-10-07 22:56:05 +0700

    Edit Scirpt use function _init_ , _str_

--- SQLInsertGenerator/sql_generator.py
+++ SQLInsertGenerator/sql_generator.py
@@ -16,6 +16,44 @@ def print_exit_program():
 	input("press any key to Exit program.")
 	sys.exit()
 
+class default_table_value :
+	def __init__(self, columnName, rowvalue , defaultUser):
+		self.columnName = str(columnName).upper()
+		self.rowvalue = rowvalue
+		self.defaultUser = str(defaultUser).upper()
+
+	def __str__(self):
+		if self.columnName == "ROW_ID" and pandas.isnull(self.rowvalue) :
+			self.rowvalue = "'"+str(uuid.uuid4().hex).upper()+"'"
+		elif self.columnName == "ACTIVE_FLG" and pandas.isnull(self.rowvalue)  :
+			self.rowvalue = "'Y'"
+		elif self.columnName == "MODIFICATION_NUM" :
+			if pandas.isnull(self.rowvalue) :
+				self.rowvalue = "1"
+		elif self.columnName == "CREATED" :
+			if pandas.isnull(self.rowvalue) :
+				self.rowvalue = "SYSDATE"
+			else :
+				self.rowvalue = "TO_DATE('"+str(self.rowvalue)+"', 'dd-mm-yyyy hh24:mi:ss')"
+		elif self.columnName == "CREATED_BY" and pandas.isnull(self.rowvalue) :
+			self.rowvalue = "'"+self.defaultUser+"'"
+		elif self.columnName == "LAST_UPD" :
+			if pandas.isnull(self.rowvalue) :
+				self.rowvalue = "SYSDATE"
+			else :
+				self.rowvalue = "TO_DATE('"+str(self.rowvalue)+"', 'dd-mm-yyyy hh24:mi:ss')"
+		elif self.columnName == "LAST_UPD_BY" and pandas.isnull(self.rowvalue) :
+			self.rowvalue = "'"+self.defaultUser+"'"
+		elif self.columnName == "GROUP_TYPE" and pandas.isnull(self.rowvalue) :
+			self.rowvalue = "'CONFIG'"
+		else :
+			if pandas.isnull(self.rowvalue) :
+				self.rowvalue = "''"
+			else :
+				self.rowvalue = "'"+str(self.rowvalue)+"'"
+		return str(self.rowvalue)
+	
+
 try :
 	fileIn : str = input("Input excel File Name (FileName.xlsx) : ")
 	print()
@@ -29,10 +67,15 @@ try :
 		print_exit_program()
 
 	file = pandas.ExcelFile(fileIn)
-	defaultUser = defaultUser.upper()
 
 	try:
 		fileName = fileIn.replace('.xlsx','')
+		fileName = fileName.replace('.xls','')
+		fileName = fileName.replace('.xlsm','')
+		fileName = fileName.replace('.xlsb','')
+		fileName = fileName.replace('.odf','')
+		fileName = fileName.replace('.ods','')
+		fileName = fileName.replace('.odt','')
 		os.makedirs(fileName)
 		outputdir = "{}/".format(fileName)
 	except OSError as exc:
@@ -49,37 +92,8 @@ try :
 			field_names = str(field_names).upper()
 			rows = list()
 			for column in data.columns:
-				columnName = str(column).upper()
-				rowvalue = data[column][i]
-				if columnName == "ROW_ID" and pandas.isnull(rowvalue) :
-					rowvalue = "'"+str(uuid.uuid4().hex).upper()+"'"
-				elif columnName == "ACTIVE_FLG" and pandas.isnull(rowvalue)  :
-					rowvalue = "'Y'"
-				elif columnName == "MODIFICATION_NUM" :
-					if pandas.isnull(rowvalue) :
-						rowvalue = "1"
-				elif columnName == "CREATED" :
-					if pandas.isnull(rowvalue) :
-						rowvalue = "SYSDATE"
-					else :
-						rowvalue = "TO_DATE('"+str(rowvalue)+"', 'dd-mm-yyyy hh24:mi:ss')"
-				elif columnName == "CREATED_BY" and pandas.isnull(rowvalue) :
-					rowvalue = "'"+defaultUser+"'"
-				elif columnName == "LAST_UPD" :
-					if pandas.isnull(rowvalue) :
-						rowvalue = "SYSDATE"
-					else :
-						rowvalue = "TO_DATE('"+str(rowvalue)+"', 'dd-mm-yyyy hh24:mi:ss')"
-				elif columnName == "LAST_UPD_BY" and pandas.isnull(rowvalue) :
-					rowvalue = "'"+defaultUser+"'"
-				elif columnName == "GROUP_TYPE" and pandas.isnull(rowvalue) :
-					rowvalue = "'CONFIG'"
-				else :
-					if pandas.isnull(rowvalue) :
-						rowvalue = "''"
-					else :
-						rowvalue = "'"+str(rowvalue)+"'"
-				rows.append(str(rowvalue))
+				defaultRowValue = default_table_value(column,data[column][i],defaultUser)
+				rows.append(str(defaultRowValue))
 			row_values = ", ".join(rows)
 			write_file.write("INSERT INTO {} ({})\nVALUES ({});\n".format(sheet_name, field_names, row_values))
 		write_file.write("COMMIT;")
